db79035da5cf34bce554580a2495d353b2370807
Ricardo <ricardo.d.lima@aluno.senai.br>
2026-02-23 08:08:09 -0300

    atividade exemplo

---
+++ MVCexemplo/Controller/UsuarioController.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start(); // "banco de dados"
+require_once "./Model/UsuarioModel.php";
+
+class UsuarioController{
+
+    public function telaCadastro(){
+        require "View/usuarioCadastrar.php";
+    }
+
+    public function cadastrar(){
+        $nome= $_POST['nome'];
+        $email= $_POST['email'];
+
+        $usuario = new Usuario($nome, $email);
+        $usuario->salvar();
+        // redirecionar ao depois de salvar
+        header('Location: /PHP_PBE_2IDS_2025/MVCExemplo/usuario/telaCadastro');
+        exit;
+    }
+
+    public function listarUsuarios(){
+        $usuarios = Usuario::listar();
+        echo "<pre>";
+        print_r($usuarios);
+        echo "</pre>";
+        require 'View/usuarioListar.php';
+    }
+}
